9f3a9d84d0e8832ddbb23b6a05f1687635cecc5d
Erik Hollensbe <erik+github@hollensbe.org>
2012-11-09 04:02:26 -0800

    small wording change for cookbook:upload

--- lib/chef-workflow/tasks/cookbook/upload.rb
+++ lib/chef-workflow/tasks/cookbook/upload.rb
@@ -2,7 +2,7 @@ require 'knife/dsl'
 require 'chef-workflow/tasks/bootstrap/knife'
 
 namespace :cookbook do
-  desc "Upload your cookbooks to the test chef server"
+  desc "Upload your cookbooks to the chef server"
   task :upload => [ "bootstrap:knife" ] do
     result = knife %W[cookbook upload -a]
     fail if result != 0
