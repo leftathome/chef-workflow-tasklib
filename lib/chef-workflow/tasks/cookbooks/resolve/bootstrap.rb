6835506ad9b9ad834cf546c081fdd9c85360d9a8
Erik Hollensbe <erik+github@hollensbe.org>
2012-11-09 12:24:14 -0800

    word wrapped some long lines

--- lib/chef-workflow/tasks/cookbooks/resolve/bootstrap.rb
+++ lib/chef-workflow/tasks/cookbooks/resolve/bootstrap.rb
@@ -1,4 +1,5 @@
-# used by resolver tasklibs to ensure their basic dependencies are met. Not to be used directly.
+# used by resolver tasklibs to ensure their basic dependencies are met. Not to
+# be used directly.
 begin
   Rake::Task["cookbooks:resolve"].clear
   Rake::Task["cookbooks:update"].clear
