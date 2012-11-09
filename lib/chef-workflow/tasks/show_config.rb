3438eea5ef6549e4188af5ce3630e3395b8a4287
Erik Hollensbe <erik+github@hollensbe.org>
2012-11-09 03:51:10 -0800

    Rake task to show the known configuration

---
+++ lib/chef-workflow/tasks/show_config.rb
@@ -0,0 +1,16 @@
+require 'chef-workflow/ip-support'
+require 'chef-workflow/knife-support'
+require 'chef-workflow/vagrant-support'
+
+desc "Show the calculated configuration for chef-workflow"
+task :show_config do
+  puts "knife:"
+  %w[cookbooks_path chef_config_path knife_config_path roles_path environments_path].each do |key|
+    puts "\t#{key}: #{KnifeSupport.send(key)}"
+  end
+
+  puts "vagrant:"
+  puts "\tip subnet (/24): #{$ip_assignment.subnet}"
+  puts "\tprison dir: #{$vagrant_support.prison_dir}"
+  puts "\tbox url: #{$vagrant_support.box_url}"
+end
