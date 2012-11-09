fcc26d33280707989b8d07752d375fabb9057dc1
Erik Hollensbe <erik+github@hollensbe.org>
2012-11-09 15:05:06 -0800

    tweaked to be a little more dynamic with respect to changing configuration

--- lib/chef-workflow/tasks/chef/show_config.rb
+++ lib/chef-workflow/tasks/chef/show_config.rb
@@ -6,7 +6,8 @@ namespace :chef do
   desc "Show the calculated configuration for chef-workflow"
   task :show_config do
     puts "knife:"
-    %w[cookbooks_path chef_config_path knife_config_path roles_path environments_path].each do |key|
+    mute = %w[knife_config_template]
+    KnifeSupport::DEFAULTS.keys.reject { |x| mute.include?(x.to_s) }.each do |key|
       puts "\t#{key}: #{KnifeSupport.send(key)}"
     end
 
