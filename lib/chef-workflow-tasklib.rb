793aaa86817e8ef9ae6e0dbacc0f7efe6c5d5547
Erik Hollensbe <erik+github@hollensbe.org>
2012-11-14 10:07:24 -0800

    first stab at an "easy require" helper.

--- lib/chef-workflow-tasklib.rb
+++ lib/chef-workflow-tasklib.rb
@@ -7,3 +7,23 @@ require 'chef-workflow/tasks/chef/environments'
 require 'chef-workflow/tasks/chef/show_config'
 require 'chef-workflow/tasks/chef/clean'
 require 'chef-workflow/tasks/test/vagrant'
+
+class Chef
+  module Workflow
+    module TaskHelper
+      def chef_workflow_task(obj)
+        require "chef-workflow/tasks/#{obj}"
+      end
+    end
+  end
+end
+
+class << eval("self", TOPLEVEL_BINDING)
+  include Chef::Workflow::TaskHelper
+end
+
+if defined? Rake::DSL
+  module Rake::DSL
+    include Chef::Workflow::TaskHelper
+  end
+end
