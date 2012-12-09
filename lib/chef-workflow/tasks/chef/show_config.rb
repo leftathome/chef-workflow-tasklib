9a1b9a686e9ff761c219c8cebef3c354bd1c3fa0
Erik Hollensbe <erik+github@hollensbe.org>
2012-12-09 00:18:03 -0800

    add ec2 config display

--- lib/chef-workflow/tasks/chef/show_config.rb
+++ lib/chef-workflow/tasks/chef/show_config.rb
@@ -10,6 +10,7 @@ namespace :chef do
     puts "\tworkflow_dir: #{GeneralSupport.singleton.workflow_dir}"
     puts "\tvm_file: #{GeneralSupport.singleton.vm_file}"
     puts "\tchef_server_prison: #{GeneralSupport.singleton.chef_server_prison}"
+    puts "\tmachine_provisoner: #{GeneralSupport.singleton.machine_provisioner}"
 
     puts "knife:"
     mute = %w[knife_config_template]
@@ -20,5 +21,18 @@ namespace :chef do
     puts "vagrant:"
     puts "\tip subnet (/24): #{IPSupport.singleton.subnet}"
     puts "\tbox url: #{VagrantSupport.singleton.box_url}"
+
+    puts "ec2:"
+
+    %w[
+      ami
+      instance_type
+      region
+      ssh_key
+      security_groups
+      security_group_open_ports
+    ].each do |key|
+      puts "\t#{key}: #{EC2Support.singleton.send(key)}"
+    end
   end
 end
