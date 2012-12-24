567ad74ea61422f5f28a90d6c0dd3acf4aa449c9
Erik Hollensbe <erik+github@hollensbe.org>
2012-12-24 04:23:33 -0800

    fit chef:build to use new with_scheduler stuff.

--- lib/chef-workflow/tasks/chef/build.rb
+++ lib/chef-workflow/tasks/chef/build.rb
@@ -13,39 +13,31 @@ namespace :chef do
       raise 'You must supply a role name to chef:build'
     end
 
-    unless File.exist?(KnifeSupport.singleton.knife_config_path)
-      raise 'Cannot find your knife.rb -- did you create a chef server?'
-    end
-  
     role_name          = args[:role_name]
     number_of_machines = (args[:number_of_machines] || 1).to_i
 
-    Chef::Config.from_file(KnifeSupport.singleton.knife_config_path)
+    with_scheduler(true) do |s|
+      kp              = VM::KnifeProvisioner.new
+      kp.username     = KnifeSupport.singleton.ssh_user
+      kp.password     = KnifeSupport.singleton.ssh_password
+      kp.use_sudo     = KnifeSupport.singleton.use_sudo
+      kp.ssh_key      = KnifeSupport.singleton.ssh_identity_file
+      kp.environment  = KnifeSupport.singleton.test_environment
 
-    s = Scheduler.new(false)
-    s.serial = true
-
-    kp              = VM::KnifeProvisioner.new
-    kp.username     = KnifeSupport.singleton.ssh_user
-    kp.password     = KnifeSupport.singleton.ssh_password
-    kp.use_sudo     = KnifeSupport.singleton.use_sudo
-    kp.ssh_key      = KnifeSupport.singleton.ssh_identity_file
-    kp.environment  = KnifeSupport.singleton.test_environment
-
-    s.schedule_provision(
-      role_name,
-      [
-        GeneralSupport.singleton.machine_provisioner.new(
-          role_name,
-          number_of_machines
-        ),
+      s.schedule_provision(
+        role_name,
+        [
+          GeneralSupport.singleton.machine_provisioner.new(
+            role_name,
+            number_of_machines
+      ),
         kp
       ],
-      []
-    )
+        []
+      )
 
-    s.run
-    s.wait_for(role_name)
-    s.write_state
+      s.run
+      s.wait_for(role_name)
+    end
   end
 end
