ddff7703b75e732ceb4114298fac00b17023ff77
Erik Hollensbe <erik+github@hollensbe.org>
2012-12-26 10:59:35 -0800

    move chef:show_config to chef:info:config

--- lib/chef-workflow/tasks/chef/info.rb
+++ lib/chef-workflow/tasks/chef/info.rb
@@ -1,5 +1,9 @@
 require 'chef-workflow/task-helpers/with_scheduler'
 require 'chef-workflow/support/ip'
+require 'chef-workflow/support/general'
+require 'chef-workflow/support/ip'
+require 'chef-workflow/support/knife'
+require 'chef-workflow/support/vagrant'
 
 namespace :chef do
   namespace :info do
@@ -25,6 +29,38 @@ namespace :chef do
         IPSupport.singleton.get_role_ips(role).each do |ip|
           puts "\t#{ip}"
         end
+      end
+    end
+
+    desc "Show the calculated configuration for chef-workflow"
+    task :config do
+      puts "general:"
+      puts "\tworkflow_dir: #{GeneralSupport.singleton.workflow_dir}"
+      puts "\tvm_file: #{GeneralSupport.singleton.vm_file}"
+      puts "\tchef_server_prison: #{GeneralSupport.singleton.chef_server_prison}"
+      puts "\tmachine_provisoner: #{GeneralSupport.singleton.machine_provisioner}"
+
+      puts "knife:"
+      mute = %w[knife_config_template]
+      KnifeSupport::DEFAULTS.keys.reject { |x| mute.include?(x.to_s) }.each do |key|
+        puts "\t#{key}: #{KnifeSupport.singleton.send(key)}"
+      end
+
+      puts "vagrant:"
+      puts "\tip subnet (/24): #{IPSupport.singleton.subnet}"
+      puts "\tbox url: #{VagrantSupport.singleton.box_url}"
+
+      puts "ec2:"
+
+      %w[
+        ami
+        instance_type
+        region
+        ssh_key
+        security_groups
+        security_group_open_ports
+      ].each do |key|
+          puts "\t#{key}: #{EC2Support.singleton.send(key)}"
       end
     end
   end
