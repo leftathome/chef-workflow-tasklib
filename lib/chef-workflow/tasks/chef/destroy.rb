cdcb4963d6f652885a62cb2393b5ef01c1ebe9da
Erik Hollensbe <erik+github@hollensbe.org>
2013-03-12 02:00:20 -0700

    chef:destroy is a new task to tear down an individual machine. resolves #5

---
+++ lib/chef-workflow/tasks/chef/destroy.rb
@@ -0,0 +1,17 @@
+require 'chef-workflow/task-helpers/with_scheduler'
+require 'chef-workflow/support/vm/helpers/knife'
+
+namespace :chef do
+  desc "Tear down a server group"
+  task :destroy, :role_name do |task, args|
+    unless args[:role_name]
+      raise 'You must supply a role name to chef:destroy'
+    end
+
+    role_name = args[:role_name]
+
+    with_scheduler do |s|
+      s.teardown_group(role_name)
+    end
+  end
+end
