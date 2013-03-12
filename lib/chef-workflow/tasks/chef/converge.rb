b376e280bc6904e647655c9ec97fbba84946b408
Erik Hollensbe <erik+github@hollensbe.org>
2013-03-12 03:11:31 -0700

    make chef:converge repeatable too.

--- lib/chef-workflow/tasks/chef/converge.rb
+++ lib/chef-workflow/tasks/chef/converge.rb
@@ -3,6 +3,8 @@ require 'chef-workflow/task-helpers/ssh'
 namespace :chef do
   desc "run chef-client on a group of machines"
   task :converge, :role_name do |task, args|
+    task.reenable
+
     unless args[:role_name]
       raise "You must supply a node name to converge"
     end
